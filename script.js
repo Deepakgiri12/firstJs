0ea458bb77d58620f9b757debde7cfbd282a790b
Deepak Giri <goswamideepak99965@gmail.com>
2025-11-05 20:02:54 +0530

    Add files via upload
    uploaded documents

---
+++ script.js
@@ -0,0 +1,28 @@
+const greet = document.querySelector(".greet-val");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+const box1=document.querySelector(".box-1");
+const box2=document.querySelector(".box-2");
+const box3=document.querySelector(".box-3");
+const box4=document.querySelector(".box-4");
+btn.addEventListener("click", () => {
+    greet.innerHTML = "Hello,"+input.value;
+});
+
+box1.addEventListener("click",()=>{
+    box1.style.backgroundColor="red";
+    box1.style.color="white";
+})
+box2.addEventListener("click",()=>{
+    box2.style.backgroundColor="blue";
+    box2.style.color="white";
+})
+box3.addEventListener("click",()=>{
+    box3.style.backgroundColor="green";
+    box3.style.color="white";
+})
+box4.addEventListener("click",()=>{
+    box4.style.backgroundColor="yellow";
+    box4.style.color="black";
+})
+
